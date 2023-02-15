1270967a0a843b30414a4392ad36cf2e9d75a856
clovis <clodo@gmail.com>
2023-02-15 16:36:12 -0300

    first commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from 'axios'
 
 const Api: AxiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api",
-    // baseURL: "https://tropical-tech.netlify.app/api",
+    // baseURL: "http://localhost:3000/api",
+    baseURL: "https://tropical-tech.netlify.app/api",
 })
 
 
